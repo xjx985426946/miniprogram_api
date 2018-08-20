59b31546acc902f2d1050b4e97b8a9aa920d76e2
Administrator <xjx985426946@163.com>
2018-08-20 21:52:45 +0800

    新增商品相关接口

---
+++ Intbee_API/Request/product/editing_product.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/17 15:54
+# @Author  : yixiong
+
+import urllib3
+import requests
+import json
+
+class editing_product():   #编辑商品
+    def editing():
+        headers = {
+            'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDEiLCJzdWIiOiI1YWMzNDRiOGJjOGU3NzAwMDVmNzJjZTkiLCJpYXQiOjE1MzM4MDk4MzA0NjEsImV4cCI6MTUzMzgzNTAzMDQ2MX0.NVJsKhRmrnve-6pMlSMto4CesFKY8N6RGpCurqzWvCY',
+            'Content-Type': 'application/json'
+            }
+        data = {
+            "merchant_uuid":"5ac344b8bc8e770005f72ce9",
+            "title": "测试22",
+            "main_image": [        
+                "http://miniprogram-static.intbee.com/FjPqv_lP8pYRGOr4pDF_CY7wu2i9",
+                "http://miniprogram-static.intbee.com/Fjc6YVEbkyO96TBkYyBLKxxv8a8K",
+                "http://miniprogram-static.intbee.com/Fq1tJDnJXmfHASHzME7B59HYzEv6"],
+            "price": "0.01",
+            "stock": "12",
+            "product_status":"1",
+            "description": "[{\"text\":\"恶魔一组图\",\"image\":\"http://miniprogram-static.intbee.com/Fjc6YVEbkyO96TBkYyBLKxxv8a8K\"}]"
+          }
+        data_json = json.dumps(data)
+        url = 'https://test-wechatapp.intbee.com/api/product/34'
+        req = requests.post(url,headers = headers,data=data_json)
+        user_text = req.text
+        user_json = json.loads(user_text)
+
+        if user_json['code'] == 0:
+            print('Ture, edit product is OK')
+        else:
+            print('Flase')
+            print(user_text)
+
+        
---
+++ Intbee_API/Request/product/update_product_status.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/17 15:54
+# @Author  : yixiong
+
+import urllib3
+import requests
+import json
+
+class update_produt_status():   #更新商品状态
+    def update_status():
+        headers = {
+            'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDEiLCJzdWIiOiI1YWMzNDRiOGJjOGU3NzAwMDVmNzJjZTkiLCJpYXQiOjE1MzM4MDk4MzA0NjEsImV4cCI6MTUzMzgzNTAzMDQ2MX0.NVJsKhRmrnve-6pMlSMto4CesFKY8N6RGpCurqzWvCY',
+
+            }
+
+        url = 'https://test-wechatapp.intbee.com/api/product/5ac344b8bc8e770005f72ce9/35/2'
+        req = requests.put(url,headers = headers)
+        user_text = req.text
+        user_json = json.loads(user_text)
+
+        if user_json['code'] == 0:
+            print('Ture, update product status is OK')
+        else:
+            print('Flase')
+            print(user_text)
+
