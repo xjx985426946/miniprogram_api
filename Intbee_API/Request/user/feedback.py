db058c0bb628e07d7b17ac7f2149f14d6148e64e
Administrator <xjx985426946@163.com>
2018-08-20 21:21:20 +0800

    新增问题反馈、消费者信息查询、管理后台问题反馈列表、关注相关接口

---
+++ Intbee_API/Request/user/feedback.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/17 15:54
+# @Author  : yixiong
+
+import urllib3
+import requests
+import json
+
+class user_feedback():    #用户反馈
+    def user_feedback():
+        headers = {
+            'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDEiLCJzdWIiOiI1YWMzNDRiOGJjOGU3NzAwMDVmNzJjZTkiLCJpYXQiOjE1MzM4MDk4MzA0NjEsImV4cCI6MTUzMzgzNTAzMDQ2MX0.NVJsKhRmrnve-6pMlSMto4CesFKY8N6RGpCurqzWvCY',
+            'Content-Type': 'application/json'
+            }
+        url = 'https://test-wechatapp.intbee.com/api/user/feedback'
+        data = {
+            'content':'cesces',
+            'userUuid':'5ac344b8bc8e770005f72ce9'
+            }
+        data_json = json.dumps(data)
+        req = requests.post(url, headers = headers, data = data_json)
+        user_text = req.text
+        user_json = json.loads(user_text)
+
+        if user_json['code'] == 0:
+            print('Ture, user feedback is OK')
+        else:
+            print('Flase')
+            print(user_text)
---
+++ Intbee_API/Request/user/users_add_follow.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/17 15:54
+# @Author  : yixiong
+
+import urllib3
+import requests
+import json
+
+class user_add_follow():   #获取用户信息
+    def add_follow():
+        headers = {
+            'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MDEiLCJzdWIiOiI1YWMzNDRiOGJjOGU3NzAwMDVmNzJjZTkiLCJpYXQiOjE1MzM4MDk4MzA0NjEsImV4cCI6MTUzMzgzNTAzMDQ2MX0.NVJsKhRmrnve-6pMlSMto4CesFKY8N6RGpCurqzWvCY'
+            }
+        url = 'https://test-wechatapp.intbee.com/api/user/5ac344b8bc8e770005f72ce9/followMerchant?uuid=5b0e5f2b53bbc10005380ca6'
+        req = requests.post(url ,headers = headers)
+        user_text = req.text
+        user_json = json.loads(user_text)
+
+        if user_json['code'] == 0:
+            print('Ture, user add follow is OK')
+
+        else:
+            print('Flase')
+            print(user_text)
+        
